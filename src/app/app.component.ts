83fdab81c6e8d07d99f95d2bc133ae6cb4f52dc0
Josue Mejia <josueomejiap@gmail.com>
2023-09-25 20:10:02 -0600

    clase 25092023

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { empleado } from './empleado.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-empleados';
+  titulo = 'Listado de Empleados';
+
+  cuadroNombre: string = "";
+  cuadroApellido: string = "";
+  cuadroCargo: string = "";
+  cuadroSalario: number = 0;
+
+  empleados : empleado[] = [
+    new empleado("Josue", "Mejia", "Presidente", 5000),
+    new empleado("David", "Rivas", "Gerente", 3000),
+    new empleado("Febe", "Martinez", "Jefa de Sección", 2500),
+    new empleado("Marvin", "Molina", "Decano", 800)
+  ];
+
+  agregar_empleado(){
+    let miEmpleado = new empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);
+
+    this.empleados.push(miEmpleado);
+  }
 }
