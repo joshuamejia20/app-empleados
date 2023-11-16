f6226003acdccb12730902faa14d93f8f67a98a5
Josue Mejia <josueomejiap@gmail.com>
2023-11-15 19:36:54 -0600

    clase 15112023

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import firebase from 'firebase/compat/app';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
+export class AppComponent implements OnInit{
 
-  
+  ngOnInit():void{
+    firebase.initializeApp({
+      apiKey: "AIzaSyDld4YoAxsMBfHAFWZZ866yo5AVAb9WqN0",
+      authDomain: "misempleados-42beb.firebaseapp.com"
+    })
+  }
   
 }
