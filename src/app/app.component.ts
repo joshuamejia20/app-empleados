21eb4f1552b6c119b51ae6e25738e998f28b10ec
Josue Mejia <josueomejiap@gmail.com>
2024-11-19 15:04:31 -0600

    clase 181124

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import firebase from 'firebase/compat/app';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,11 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    firebase.initializeApp({
+      apiKey: "AIzaSyDctzmPXQXCdeK3GFEXpvifoF80zsPfNgo",
+      authDomain: "misempleados2024.firebaseapp.com"
+    });
+  }
 }
