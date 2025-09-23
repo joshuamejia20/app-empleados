ebe8907c487dfeca23c04c21ef5a865611dbdcd5
Josue Mejia <josueomejiap@gmail.com>
2025-09-22 19:29:18 -0600

    clase 22092025

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('app-empleados');
+  titulo = "Listado de Empleados";
 }
