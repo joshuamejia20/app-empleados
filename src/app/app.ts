54f2c516164ba45be52a2300a80d9d0834edc8d4
Josue Mejia <josueomejiap@gmail.com>
2025-09-24 19:35:21 -0600

    clase 240925

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,32 @@
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { empleado } from './empleado.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   titulo = "Listado de Empleados";
+  cuadroNombre: string = "";
+  cuadroApellido: string = "";
+  cuadroCargo: string = "";
+  cuadroSalario: number = 0;
+
+  empleados: empleado[] = [
+    new empleado("Josue", "Mejia", "Jefe", 99999.99),
+    new empleado("Jared", "Garay", "Director", 1000),
+    new empleado("Juan", "Pérez", "Administrativo", 800),
+    new empleado("Maria", "Sorto", "Colaboradora", 500)
+  ];
+
+  agregar_empleado(){
+    let miEmpleado = new empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);
+
+    this.empleados.push(miEmpleado);
+  }
 }
